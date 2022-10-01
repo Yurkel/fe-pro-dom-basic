265b9737ceb95fdb95d92cb5e106aa49c52240f0
Yuriy <yuriy-k@ukr.net>
2022-10-01 17:42:22 +0300

    all done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  const arrLi = document.querySelectorAll('li');
+  for (let i = 0; i <= arrLi.length; i++) {
+    if (i % 2 === 0) {
+      arrLi[i].style.backgroundColor = 'red';
+    }
+  }
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+  let el = document.querySelector('li');
+  while (el) {
+    if (el.classList.contains('likedItem')) {
+      break;
+    }
+    el = el.nextElementSibling;
+  }
+  el.style.backgroundColor = 'blue';
+};
